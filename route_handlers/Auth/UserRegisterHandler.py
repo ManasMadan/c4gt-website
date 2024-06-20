0261cf4336589782d3eb80b4465ad0b8bbdf50c4
Manas Madan <manasmadan08@gmail.com>
2024-06-20 21:08:56 +0530

    register,forgot,login working

--- route_handlers/Auth/UserRegisterHandler.py
+++ route_handlers/Auth/UserRegisterHandler.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for, make_response
 import logging
-import cloud
+from cloud.authenticate.user import user_exists,create_user
 
 class UserRegisterHandler:
     @staticmethod
@@ -16,10 +16,10 @@ class UserRegisterHandler:
         password = request.form.get('password')
         logging.info(user)
         logging.info(password)
-        if cloud.authenticate.user.user_exists(user):
+        if user_exists(user):
             # User already exists
             return render_template("userregister-exists.html", user=None, reguser=user)
-        cloud.authenticate.user.create_user(user, password)
+        create_user(user, password)
         response = make_response(render_template("userregister-ok.html", user=user))
         response.set_cookie('user', user)
         return response
